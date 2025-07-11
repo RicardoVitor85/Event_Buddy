193ac5cd257d815881eaa47a5c6f2a1c8eb57ff6
RicardoVitor85 <rikardo.lrs.85@live.com.pt>
2025-07-11 15:17:13 +0100

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAf0GZ99dbEZWSpBX04wDOldEMvi_01g7g',
-  authDomain: 'eventbuddy-1c2c3.firebaseapp.com',
-  projectId: 'eventbuddy-1c2c3',
-  storageBucket: 'eventbuddy-1c2c3.firebasestorage.app',
-  messagingSenderId: '83395308571',
-  appId: '1:83395308571:web:34faca00b51d7acb8d5cbe',
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
 };
 
 if (!firebase.apps.length){ 
